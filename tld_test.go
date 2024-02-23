c13888d9ccf363b3121b6521b9b6e76e0ea7248b
Hazem El-Sayed <118324667+zomasec@users.noreply.github.com>
2024-02-23 11:52:18 +0200

    Create tld_test.go

---
+++ tld_test.go
@@ -0,0 +1,39 @@
+package tld
+
+import (
+	"fmt"
+	"testify/assert"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	t.Run("Case have one subdomain", func(t *testing.T) {
+		want := "subdomain1"
+		parsed, _ := Parse("https://subdomain1.example.com.eg")
+		got := parsed.Subdomains[0]
+		assert.Equalf(t, want, got, "want %s but got %s", want, got)
+	})
+
+	t.Run("Case have more than subdomain", func(t *testing.T) {
+		want := []string{"subdomain1", "subdomain2", "subdomain3"}
+		parsed, _ := Parse("subdomain1.subdomain2.subdomain3.example.com")
+		got := parsed.Subdomains
+		fmt.Println(got)
+		assert.ElementsMatch(t, want, got, "want %s but got %s", want, got)
+
+	})
+
+	t.Run("Case i want tld", func(t *testing.T) {
+		want := "com"
+		parsed, _ := Parse("https://subdomain1.example.com")
+		got := parsed.TLD
+		assert.Equalf(t, want, got, "want %s but got %s", want, got)
+
+	})
+
+	t.Run("Case invalid url", func(t *testing.T) {
+		_, err := Parse("subdomain1example")
+		assert.Error(t, err)
+
+	})
+}
